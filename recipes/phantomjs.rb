78ead5e9f0e35463da32deab538d8a2722fcc885
Frank Hoffsümmer <frank.hoffsummer@gmail.com>
2013-01-05 21:58:33 +0100

    silencing output

--- recipes/phantomjs.rb
+++ recipes/phantomjs.rb
@@ -1,7 +1,6 @@
 #--------
 # install phantomjs
 #--------
-# from http://skookum.com/blog/dynamic-screenshots-on-the-server-with-phantomjs/
 remote_file "/tmp/phantomjs-#{node["webscreenshots"]["phantomjs"]["version"]}.tar.bz2" do
   source node["webscreenshots"]["phantomjs"]["uri"]
   mode "0644"
@@ -13,7 +12,7 @@ bash "install phantomjs" do
   cwd "/tmp"
   user "root"
   code <<-EOS
-    tar -jxvf phantomjs-#{node["webscreenshots"]["phantomjs"]["version"]}.tar.bz2
+    tar -jxvf phantomjs-#{node["webscreenshots"]["phantomjs"]["version"]}.tar.bz2 >/dev/null
     mv phantomjs-#{node["webscreenshots"]["phantomjs"]["version"]}-linux-x86_64 /opt
   EOS
   not_if "test -e /opt/phantomjs-#{node["webscreenshots"]["phantomjs"]["version"]}-linux-x86_64"
