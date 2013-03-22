85dd7c1dcd2b7b4fe9a8804a199c7c424703058f
Frank Hoffsümmer <frank.hoffsummer@gmail.com>
2013-03-22 14:41:14 +0100

    removing boto.cfg

--- recipes/supervisord.rb
+++ recipes/supervisord.rb
@@ -19,12 +19,3 @@ service "supervisor" do
   supports :status => true, :restart => true
   action [:enable, :start]
 end
-
-#TODO: ~/.boto.cfg
-#template "#{node["webscreenshots"]["home"]}/.boto.cfg" do
-# from http://stackoverflow.com/a/9197801/41404
-template "/etc/boto.cfg" do
-  source "boto.cfg.erb"
-  mode 0644
-  notifies :restart, "service[supervisor]"
-end
