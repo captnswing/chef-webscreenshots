eb2ed5646a1093d92823cb9dd6867c175d42f3a3
Frank Hoffsümmer <frank.hoffsummer@gmail.com>
2013-03-25 20:36:34 +0100

    removing umlaut. for now

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,12 @@
 name 'webscreenshots'
-maintainer 'Frank Hoffsümmer'
+maintainer 'Frank Hoffsummer'
 maintainer_email 'frank.hoffsummer@gmail.com'
 license 'All rights reserved'
 description 'Installs and configures webscreenshots application'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.0.4'
 
+depends 'runit'
 depends 'python'
 depends 'postgresql'
 depends 'nginx'
