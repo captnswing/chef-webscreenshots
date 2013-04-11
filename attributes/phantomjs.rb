73e98c5da44b8f183ee937c3d7f26a59e8842bae
Frank Hoffsümmer <frank.hoffsummer@gmail.com>
2013-04-11 22:10:49 +0200

    bumped phantomjs version

--- attributes/phantomjs.rb
+++ attributes/phantomjs.rb
@@ -1,3 +1,3 @@
-default["webscreenshots"]["phantomjs"]["version"] = "1.8.1"
+default["webscreenshots"]["phantomjs"]["version"] = "1.9.0"
 default["webscreenshots"]["phantomjs"]["arch"] = "x86_64" # or "i686"
 default["webscreenshots"]["phantomjs"]["uri"] = "http://phantomjs.googlecode.com/files/phantomjs-#{node["webscreenshots"]["phantomjs"]["version"]}-linux-#{node["webscreenshots"]["phantomjs"]["arch"]}.tar.bz2"
